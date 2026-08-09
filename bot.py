cbad4118ac639b5b053ba53bab3686fd1bfa1ecd
acongkentong-cloud <acongkentong@gmail.com>
2026-08-09 12:42:05 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ DATA_FILE = "bot_data.json"
 USER_NAME_TAGS = {
     "@lubu_hiat": "LUBU PAKAM",
     "@max77kix": "MAX",
-    "@Zkyy07": "SELLER GOOGLE ADS",
+    "@Zkyy07": "ADS",
     "@tiliqua_gigas": "TILIQUA_AG",
     # "@username_lain": "NAME TAG",
 }
